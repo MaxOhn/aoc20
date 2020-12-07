c95716272bf0b5e862c0b5fbf2197f28380dc973
MaxOhn <ohn.m@hotmail.de>
2020-12-07 15:11:22 +0100

    day07: bunch of optimizations

--- day07/src/main.rs
+++ day07/src/main.rs
@@ -1,14 +1,12 @@
-use std::borrow::Borrow;
 use std::collections::HashMap;
 use std::io::{BufRead, BufReader};
-use std::rc::Rc;
 use std::time::Instant;
 
-type Bags = HashMap<Key, Vec<BagAmount>>;
-type CachePart1 = HashMap<Key, bool>;
-type CachePart2 = HashMap<Key, usize>;
+type Bags = HashMap<u16, Vec<(u8, u16)>>;
+type CachePart1 = HashMap<u16, bool>;
+type CachePart2 = HashMap<u16, u32>;
 
-const MY_BAG: &str = "shiny gold";
+const MY_BAG: &str = "iny go"; // always omit the first and last two characters
 
 fn main() {
     let start = Instant::now();
@@ -18,162 +16,121 @@ fn main() {
     let mut line = String::new();
 
     let mut names = HashMap::with_capacity(594);
-    let mut bags = HashMap::with_capacity(594);
+    names.insert(MY_BAG.to_owned(), 0);
+    let mut id = 1;
+
+    let mut bags = HashMap::with_capacity(590);
 
     while input.read_line(&mut line).unwrap() != 0 {
         let bytes = line.as_bytes();
+        let i = bag_end_idx(bytes) - 2;
 
-        let i = bag_end_idx(bytes);
-
-        if unsafe { line.get_unchecked(i + 14..) }.starts_with("no ") {
+        if unsafe { *bytes.get_unchecked(i + 16) } == b'n' {
             line.clear();
             continue;
         }
 
-        let outer = if let Some(bag) = names.get(unsafe { line.get_unchecked(..i) }) {
-            Rc::clone(bag)
+        let outer = if let Some(bag) = names.get(unsafe { line.get_unchecked(2..i) }) {
+            *bag
         } else {
-            let bag = Rc::new(unsafe { line.get_unchecked(..i) }.to_owned());
-            names.insert(Key(Rc::clone(&bag)), Rc::clone(&bag));
+            names.insert(unsafe { line.get_unchecked(2..i) }.to_owned(), id);
+            id += 1;
 
-            bag
+            id - 1
         };
 
-        for inner in unsafe { line.get_unchecked(i + 14..) }.split(", ") {
+        for inner in unsafe { line.get_unchecked(i + 16..) }.split(", ") {
             let bytes = inner.as_bytes();
             let n = unsafe { *bytes.get_unchecked(0) } - b'0';
+            let i = bag_end_idx(unsafe { bytes.get_unchecked(4..) }) + 2;
 
-            let bytes = match unsafe { inner.get_unchecked(2..) }.find('.') {
-                Some(idx) => unsafe { bytes.get_unchecked(2..idx) },
-                None => unsafe { bytes.get_unchecked(2..) },
-            };
-
-            let i = bag_end_idx(bytes);
-
-            let inner = if let Some(bag) = names.get(unsafe { inner.get_unchecked(2..i + 2) }) {
-                Rc::clone(bag)
+            let inner = if let Some(bag) = names.get(unsafe { inner.get_unchecked(4..i) }) {
+                (n, *bag)
             } else {
-                let bag = Rc::new(unsafe { inner.get_unchecked(2..i + 2).to_owned() });
-                names.insert(Key(Rc::clone(&bag)), Rc::clone(&bag));
+                names.insert(unsafe { inner.get_unchecked(4..i).to_owned() }, id);
+                id += 1;
 
-                bag
+                (n, id - 1)
             };
 
-            let inner = BagAmount::new(n, inner);
-
-            bags.entry(Key(Rc::clone(&outer)))
-                .or_insert_with(|| Vec::with_capacity(1))
+            bags.entry(outer)
+                .or_insert_with(|| Vec::with_capacity(2))
                 .push(inner);
         }
 
         line.clear();
     }
 
-    println!("Setup: {:?}", start.elapsed()); // 950µs
+    println!("Setup: {:?}", start.elapsed()); // 850µs
 
     let start = Instant::now();
-    let mut cache = HashMap::with_capacity(128);
+    let mut cache = HashMap::with_capacity(256);
 
     let p1 = bags
         .keys()
-        .filter(|bag| contains_recursive(&bag.0, &bags, &mut cache))
-        .count();
+        .filter(|&&bag| contains_recursive(bag, &bags, &mut cache))
+        .count()
+        - 1; // my bag does not contain itself
 
-    println!("Part 1: {} [{:?}]", p1, start.elapsed()); // 250µs
+    println!("Part 1: {} [{:?}]", p1, start.elapsed()); // 110µs
 
     let start = Instant::now();
-    let mut cache = HashMap::with_capacity(128);
+    let mut cache = HashMap::with_capacity(64);
 
-    let p2: usize = bags
-        .get(MY_BAG)
+    let p2: u32 = bags
+        .get(&0)
         .unwrap()
         .iter()
-        .map(|BagAmount { amount, bag }| *amount as usize * count_recursive(bag, &bags, &mut cache))
+        .map(|(amount, bag)| *amount as u32 * count_recursive(*bag, &bags, &mut cache))
         .sum();
 
-    println!("Part 2: {} [{:?}]", p2, start.elapsed()); // 9µs
+    println!("Part 2: {} [{:?}]", p2, start.elapsed()); // 6µs
 
     assert_eq!(p1, 161);
     assert_eq!(p2, 30_899);
 }
 
-fn contains_recursive(bag: &Rc<String>, bags: &Bags, cache: &mut CachePart1) -> bool {
-    let key = Key(Rc::clone(bag));
-
-    if let Some(value) = cache.get(bag) {
+fn contains_recursive(bag: u16, bags: &Bags, cache: &mut CachePart1) -> bool {
+    let inner = if let Some(value) = cache.get(&bag) {
         return *value;
-    } else if bag.as_ref() == MY_BAG {
+    } else if bag == 0 {
+        return true;
+    } else if let Some(bag) = bags.get(&bag) {
+        bag
+    } else {
         return false;
-    }
-
-    let inner = match bags.get(bag) {
-        Some(bag) => bag,
-        None => return false,
     };
 
-    if inner.iter().any(|bag| bag.bag.as_ref() == MY_BAG) {
-        cache.insert(key, true);
-        return true;
-    }
-
-    for BagAmount { bag, .. } in inner {
-        if contains_recursive(bag, bags, cache) {
-            cache.insert(key, true);
+    for (_, bag) in inner {
+        if contains_recursive(*bag, bags, cache) {
+            cache.insert(*bag, true);
             return true;
         }
     }
 
-    cache.insert(key, false);
+    cache.insert(bag, false);
     false
 }
 
-fn count_recursive(bag: &Rc<String>, bags: &Bags, cache: &mut CachePart2) -> usize {
-    let key = Key(Rc::clone(bag));
-
-    if let Some(value) = cache.get(bag) {
+fn count_recursive(bag: u16, bags: &Bags, cache: &mut CachePart2) -> u32 {
+    if let Some(value) = cache.get(&bag) {
         return *value;
     }
 
     let mut count = 1;
 
-    if let Some(inner) = bags.get(bag) {
-        for BagAmount { amount, bag } in inner {
-            count += *amount as usize * count_recursive(bag, bags, cache);
+    if let Some(inner) = bags.get(&bag) {
+        for (amount, bag) in inner {
+            count += *amount as u32 * count_recursive(*bag, bags, cache);
         }
     }
 
-    cache.insert(key, count);
+    cache.insert(bag, count);
     count
 }
 
-struct BagAmount {
-    amount: u8,
-    bag: Rc<String>,
-}
-
-impl BagAmount {
-    fn new(amount: u8, bag: Rc<String>) -> Self {
-        Self { amount, bag }
-    }
-}
-
-#[derive(Eq, PartialEq, Hash)]
-struct Key(Rc<String>);
-
-impl Borrow<str> for Key {
-    fn borrow(&self) -> &str {
-        self.0.as_ref()
-    }
-}
-
-impl Borrow<Rc<String>> for Key {
-    fn borrow(&self) -> &Rc<String> {
-        &self.0
-    }
-}
-
 fn bag_end_idx(bag: &[u8]) -> usize {
-    let mut i = 0;
+    let mut i = 1;
     let mut found_first = false;
 
     loop {
